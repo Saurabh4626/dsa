c7f8d2e837f3b8cccd879a13e61e4fa4f4b90bd3
Saurabh4626 <60349103+Saurabh4626@users.noreply.github.com>
2021-02-01 12:10:26 +0530

    Add files via upload

---
+++ dictionary/Print_all_words_with_frequency{K}.py
@@ -0,0 +1,22 @@
+a="this is string of word it can contain many many word"
+k=2
+words=a.split() ##list of all words
+d={}
+
+## one way of creating dictionary of frequency of words
+
+# for w in words:
+#     if w in d:
+#         d[w]+=1
+#     else:
+#         d[w]=1
+
+## one way of creating dictionary of frequency of words
+
+for w in words:
+    d[w]=d.get(w,0)+1
+print(d)
+
+for i in d:
+    if d[i]==k:
+        print(i)
