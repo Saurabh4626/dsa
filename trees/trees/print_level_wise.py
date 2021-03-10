aa14c3a5b287ce0ddf2af47d17e8626f9d07b014
Saurabh Tiwari <60349103+Saurabh4626@users.noreply.github.com>
2021-03-10 22:01:58 +0530

    Add files via upload

---
+++ trees/trees/print_level_wise.py
@@ -0,0 +1,62 @@
+import queue
+class BinaryTreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+def printTreeDetailed(root):
+    if root == None:
+        return
+    print(root.data,end=":")
+    if root.left !=None:
+        print("L",root.left.data, end=",")
+    if root.right !=None:
+        print("R",root.right.data)
+    print()
+    printTreeDetailed(root.left)
+    printTreeDetailed(root.right)
+
+def levelwise_input():
+    q=queue.Queue()
+    print("enter root")
+    rootData=int(input())
+    if (rootData == -1):  ##this means user wants empty tree
+        return None
+    root=BinaryTreeNode(rootData)
+    q.put(root)
+    while (not(q.empty())):
+        current_node=q.get()
+        print("enter left nodes of {current_node} " 
+              " If no node of this " 
+              "then enter -1".format(current_node=current_node.data))
+        left_node_data=int(input())
+        if left_node_data !=-1:
+            left_node = BinaryTreeNode(left_node_data)
+            current_node.left=left_node
+            q.put(left_node)
+        print("enter right nodes of {current_node} "
+              " If no node of this "
+              "then enter -1".format(current_node=current_node.data))
+        right_node_data = int(input())
+        if right_node_data != -1:
+            right_node = BinaryTreeNode(right_node_data)
+            current_node.right = right_node
+            q.put(right_node)
+    return root
+
+def print_level_wise(root):
+    q=queue.Queue()
+    if root is None:
+        return
+    q.put(root)
+    while (not(q.empty())):
+        curr_node=q.get()
+        print(curr_node.data)
+        if curr_node.left is not None:
+            q.put(curr_node.left)
+        if curr_node.right is not None:
+            q.put(curr_node.right)
+root=levelwise_input()
+print_level_wise(root)
+
