c67344ea236589a70ba7f535ae9aed2f5f9b0125
Saurabh Tiwari <60349103+Saurabh4626@users.noreply.github.com>
2021-03-22 20:27:36 +0530

    Add files via upload

--- Classes_and_Object/inheritance3[multiple inheritance].py
+++ Classes_and_Object/inheritance3[multiple inheritance].py
@@ -8,16 +8,30 @@
 
 
 class class1:
+    def __init__(self):
+        self.name="class1"
+        super().__init__()
     def print(self):
-        print("class 1")
+        print(self.name)
 class class2:
+    def __init__(self):
+        self.name="class2"
+        super().__init__()
     def print(self):
-        print("class 2")
+        print(self.name)
+
 class class3:
+    def __init__(self):
+        self.name="class3"
+        super().__init__()
     def print(self):
-        print("class 3")
+        print(self.name)
 class child(class1,class2,class3):
-    pass
+    def __init__(self):
+        super().__init__()
+    def print(self):
+        print(self.name)
+
 c=child()
 c.print()
 
