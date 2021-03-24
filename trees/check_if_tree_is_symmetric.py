8ae389ad0b3e5f768c7fc721efd7aa19d959e721
Saurabh Tiwari <60349103+Saurabh4626@users.noreply.github.com>
2021-03-24 10:10:15 +0530

    Add files via upload

---
+++ trees/check_if_tree_is_symmetric.py
@@ -0,0 +1,62 @@
+import queue
+class BinaryTreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+def printTreeDetailed(root):
+    if root == None:
+        return
+    print(root.data,end=":")
+    if root.left !=None:
+        print("L",root.left.data, end=",")
+    if root.right !=None:
+        print("R",root.right.data)
+    print()
+    printTreeDetailed(root.left)
+    printTreeDetailed(root.right)
+
+def levelwise_input():
+    q=queue.Queue()
+    print("enter root")
+    rootData=int(input())
+    if (rootData == -1):  ##this means user wants empty tree
+        return None
+    root=BinaryTreeNode(rootData)
+    q.put(root)
+    while (not(q.empty())):
+        current_node=q.get()
+        print("enter left nodes of {current_node} " 
+              " If no node of this " 
+              "then enter -1".format(current_node=current_node.data))
+        left_node_data=int(input())
+        if left_node_data !=-1:
+            left_node = BinaryTreeNode(left_node_data)
+            current_node.left=left_node
+            q.put(left_node)
+        print("enter right nodes of {current_node} "
+              " If no node of this "
+              "then enter -1".format(current_node=current_node.data))
+        right_node_data = int(input())
+        if right_node_data != -1:
+            right_node = BinaryTreeNode(right_node_data)
+            current_node.right = right_node
+            q.put(right_node)
+    return root
+
+def is_identical(root1,root2):
+    if root1 is None and root2 is None:
+        return True
+    if root1 is None and root2 is not None:
+        return False
+    if root1 is not None and root2 is None:
+        return False
+    if ( root1.data == root2.data and is_identical(root1.left,root2.right) and is_identical(root1.right,root2.left)):
+        return True
+    return False
+def is_symmetric(root):
+    return is_identical(root.left,root.right)
+root=levelwise_input()
+print(is_symmetric(root))
+
