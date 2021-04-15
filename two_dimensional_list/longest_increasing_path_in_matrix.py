53dc7e5d51310d3db188747b8634db8ac113a44e
Saurabh Tiwari <60349103+Saurabh4626@users.noreply.github.com>
2021-04-15 19:02:51 +0530

    Add files via upload

---
+++ two_dimensional_list/longest_increasing_path_in_matrix.py
@@ -0,0 +1,14 @@
+def longestIncreasingPath(matrix) -> int:
+    row, col = len(matrix), len(matrix[0])
+    dp = [[0 for x in range(col)] for _ in range(row)]
+
+    def longest(i, j):
+        if not dp[i][j]:
+            all_path = []
+            for x, y in ((i + 1, j), (i, j + 1), (i - 1, j), (i, j - 1)):
+                if 0 <= x < row and 0 <= y < col and matrix[x][y] > matrix[i][j]:
+                    all_path.append(longest(x, y))
+            dp[i][j] = 1 + max(all_path, default=0)
+        return dp[i][j]
+
+    return max(longest(i, j) for i in range(row) for j in range(col))
